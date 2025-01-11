e77793e9e318fbb0708f8141f51b666b70895059
Paolo Benincasa <paolobenincasa1984@gmail.com>
2025-01-11 15:08:35 +0100

    inizio navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,22 +11,26 @@ import notification_icon from '../../assets/notification.png'
 function Navbar() {
   return (
     <nav className='flex-div'>
-        <div className='nav-left flex-div'>
-            <img className='menu-icon' src={menu_icon} alt="" />
-            <img className='logo' src={logo} alt="" />
-        </div>
+      <div className='nav-left flex-div'>
+        <img className='menu-icon' src={menu_icon} alt="" />
+        {/* <img className='logo' src={logo} alt="" /> */}
+        <h1 className='logo'>Tellyy</h1>
+      </div>
 
-        <div className='nav-middle flex-div'>
-            <input type="text" placeholder="Search"  />
-            <img src={search_icon} alt="" />
-        </div>
+      <div className='nav-middle flex-div'>
+        <div className='search-box flex-div'>
 
-        <div className='nav-right flex-div'>
-            <img src={upload_icon} alt="" />
-            <img src={more_icon} alt="" />
-            <img src={notification_icon} alt="" />
-            <img src={profile_icon} className='user-icon' alt="" />
+          <input type="text" placeholder="Search" />
+          <img src={search_icon} alt="" />
         </div>
+      </div>
+
+      <div className='nav-right flex-div'>
+        <img src={upload_icon} alt="" />
+        <img src={more_icon} alt="" />
+        <img src={notification_icon} alt="" />
+        <img src={profile_icon} className='user-icon' alt="" />
+      </div>
     </nav>
   )
 }
